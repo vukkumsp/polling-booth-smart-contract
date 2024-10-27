b118d190ad6b86281f08cf98c9697296563870fa
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-10-27 14:52:27 +0530

    deployment related test cases are done;

--- test/PollingBoothContract.js
+++ test/PollingBoothContract.js
@@ -6,5 +6,33 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("PollingBoothContract", function () {
+    async function deployPollingBoothContractFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
 
+        const PollingBooth = await ethers.getContractFactory("PollingBoothContract");
+        const pollingBooth = await PollingBooth.deploy();
+
+        return { pollingBooth, owner, otherAccount };
+    }
+
+    describe("Deployment", function () {
+        it("Should have correct name of deployed contract", async function () {
+          const { pollingBooth } = await loadFixture(deployPollingBoothContractFixture);
+    
+          expect(await pollingBooth.name()).to.equal("Polling Booth Contract");
+        });
+    
+        it("Should set the right owner", async function () {
+          const { pollingBooth, owner } = await loadFixture(deployPollingBoothContractFixture);
+    
+          expect(await pollingBooth.owner()).to.equal(owner.address);
+        });
+
+        it("Should have 0 eventCount", async function () {
+            const { pollingBooth } = await loadFixture(deployPollingBoothContractFixture);
+
+            expect(await pollingBooth.eventCount()).to.equal(0);
+        });
+      });
 });

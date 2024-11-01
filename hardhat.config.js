d395c39f9567d268b9103d0943c0a87327a6d5fa
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-11-01 12:27:31 +0530

    deployment steps updated; need to update again after actual contract deployment happens on sepolia testnet;

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const PRIVATE_KEYS = process.env.PRIVATE_KEYS;
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545', // Local network URL
+    },
+    sepolia: {
+      url: `https://arbitrum-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: PRIVATE_KEYS.split(","),
+    },
+  },
 };
